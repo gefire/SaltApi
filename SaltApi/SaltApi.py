05fd183b9a347646759dce5e19bff80f24522b0f
gefire <gefire@qq.com>
2017-06-23 14:34:11 +0800

    实现run, login 接口

---
+++ SaltApi/SaltApi.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###
+# author: gefire@qq.com
+###
+import requests
+import json
+
+
+class SaltApi():
+    def __init__(self, host, username, password, auth="pam"):
+        self.salt_api_host = host
+        self.username = username
+        self.passoword = password
+        self.auth = auth
+        self.headers = {
+            'Accept': 'application/json',
+            "Content-type": "application/json"
+        }
+        self.post_data = ""
+
+    def do_post(self, api="login"):
+        post_req = requests.post("%s/%s" % (self.salt_api_host, api), data=json.dumps(self.post_data),
+                                 headers=self.headers)
+        return post_req.json()["return"][0]
+
+    def login(self):
+        self.post_data = {
+            "username": self.username,
+            "password": self.passoword,
+            "eauth": self.auth
+        }
+        resp = self.do_post()
+        self.token = resp["token"]
+        self.expire = resp["expire"]
+        return self.token
+
+    def run(self, cmd="test.ping", target="*"):
+        self.post_data = [
+            {
+                "client": "local",
+                "tgt": target,
+                "fun": cmd,
+                "username": self.username,
+                "password": self.passoword,
+                "eauth": self.auth
+            }
+        ]
+        return self.do_post("run")
